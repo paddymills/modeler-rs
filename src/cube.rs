35f35d297d8dec42c65b2a7de0397c80b7da9b60
Patrick Miller <paddymills@proton.me>
2023-11-13 22:16:21 -0500

    reduce shaders

--- src/cube.rs
+++ src/cube.rs
@@ -15,111 +15,40 @@ impl ApplicationContext for Application {
 
     fn new(display: &Display<WindowSurface>) -> Self {
         let vertex_buffer = support::load_wavefront(&display, include_bytes!("obj/cube.obj"));
-        let program = program!(display,
-            140 => {
-                vertex: "
-                    #version 140
+        let vertex_shader_src = r#"
+            #version 140
 
-                    uniform mat4 persp_matrix;
-                    uniform mat4 view_matrix;
+            uniform mat4 persp_matrix;
+            uniform mat4 view_matrix;
 
-                    in vec3 position;
-                    in vec3 normal;
-                    out vec3 v_position;
-                    out vec3 v_normal;
+            in vec3 position;
+            in vec3 normal;
+            out vec3 v_position;
+            out vec3 v_normal;
 
-                    void main() {
-                        v_position = position;
-                        v_normal = normal;
-                        gl_Position = persp_matrix * view_matrix * vec4(v_position * 0.005, 1.0);
-                    }
-                ",
+            void main() {
+                v_position = position;
+                v_normal = normal;
+                gl_Position = persp_matrix * view_matrix * vec4(v_position * 0.005, 1.0);
+            }
+        "#;
 
-                fragment: "
-                    #version 140
+        let fragment_shader_src = r#"
+            #version 140
 
-                    in vec3 v_normal;
-                    out vec4 f_color;
+            in vec3 v_normal;
+            out vec4 f_color;
 
-                    const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
+            const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
 
-                    void main() {
-                        float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
-                        vec3 color = (0.3 + 0.7 * lum) * vec3(0.6, 0.0, 0.0);
-                        f_color = vec4(color, 1.0);
-                    }
-                ",
-            },
-
-            110 => {
-                vertex: "
-                    #version 110
-
-                    uniform mat4 persp_matrix;
-                    uniform mat4 view_matrix;
-
-                    attribute vec3 position;
-                    attribute vec3 normal;
-                    varying vec3 v_position;
-                    varying vec3 v_normal;
-
-                    void main() {
-                        v_position = position;
-                        v_normal = normal;
-                        gl_Position = persp_matrix * view_matrix * vec4(v_position * 0.005, 1.0);
-                    }
-                ",
-
-                fragment: "
-                    #version 110
-
-                    varying vec3 v_normal;
-
-                    const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
-
-                    void main() {
-                        float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
-                        vec3 color = (0.3 + 0.7 * lum) * vec3(1.0, 1.0, 1.0);
-                        gl_FragColor = vec4(color, 1.0);
-                    }
-                ",
-            },
-
-            100 => {
-                vertex: "
-                    #version 100
-
-                    uniform lowp mat4 persp_matrix;
-                    uniform lowp mat4 view_matrix;
-
-                    attribute lowp vec3 position;
-                    attribute lowp vec3 normal;
-                    varying lowp vec3 v_position;
-                    varying lowp vec3 v_normal;
-
-                    void main() {
-                        v_position = position;
-                        v_normal = normal;
-                        gl_Position = persp_matrix * view_matrix * vec4(v_position * 0.005, 1.0);
-                    }
-                ",
-
-                fragment: "
-                    #version 100
-
-                    varying lowp vec3 v_normal;
-
-                    const lowp vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
-
-                    void main() {
-                        lowp float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
-                        lowp vec3 color = (0.3 + 0.7 * lum) * vec3(1.0, 1.0, 1.0);
-                        gl_FragColor = vec4(color, 1.0);
-                    }
-                ",
-            },
-        )
-        .unwrap();
+            void main() {
+                float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
+                vec3 color = (0.3 + 0.7 * lum) * vec3(0.89, 0.51, 0.49);
+                f_color = vec4(color, 1.0);
+            }
+        "#;
+        let program = glium::Program::from_source(display, vertex_shader_src, fragment_shader_src,
+            None).unwrap();
 
         let camera = support::camera::CameraState::new();
 
@@ -148,7 +77,7 @@ impl ApplicationContext for Application {
             ..Default::default()
         };
 
-        frame.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+        frame.clear_color_and_depth((0.18, 0.25, 0.4, 1.0), 1.0);
         frame
             .draw(
                 &self.vertex_buffer,
