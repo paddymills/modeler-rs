9c1f5c54e2bba85fff598c522e53b35d25b25dfc
paddymills <paddymills@pm.me>
2023-11-14 10:40:59 -0500

    move shaders to files

--- src/cube.rs
+++ src/cube.rs
@@ -15,38 +15,9 @@ impl ApplicationContext for Application {
 
     fn new(display: &Display<WindowSurface>) -> Self {
         let vertex_buffer = support::load_wavefront(&display, include_bytes!("obj/cube.obj"));
-        let vertex_shader_src = r#"
-            #version 140
 
-            uniform mat4 persp_matrix;
-            uniform mat4 view_matrix;
-
-            in vec3 position;
-            in vec3 normal;
-            out vec3 v_position;
-            out vec3 v_normal;
-
-            void main() {
-                v_position = position;
-                v_normal = normal;
-                gl_Position = persp_matrix * view_matrix * vec4(v_position * 0.005, 1.0);
-            }
-        "#;
-
-        let fragment_shader_src = r#"
-            #version 140
-
-            in vec3 v_normal;
-            out vec4 f_color;
-
-            const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
-
-            void main() {
-                float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
-                vec3 color = (0.3 + 0.7 * lum) * vec3(0.89, 0.51, 0.49);
-                f_color = vec4(color, 1.0);
-            }
-        "#;
+        let vertex_shader_src   = include_str!("shaders/vertex.glsl");
+        let fragment_shader_src = include_str!("shaders/fragment.glsl");
         let program = glium::Program::from_source(display, vertex_shader_src, fragment_shader_src,
             None).unwrap();
 
