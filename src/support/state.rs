04947927e56394281e2bbdc853ba1f0c7c71596c
paddymills <paddymills@pm.me>
2023-11-15 11:42:43 -0500

    refactor remove state as Option

--- src/support/state.rs
+++ src/support/state.rs
@@ -24,6 +24,7 @@ pub struct State<T> {
     pub display: Display<WindowSurface>,
     pub window: Window,
     pub context: T,
+    active: bool
 }
 
 impl<T: ApplicationContext + 'static> State<T> {
@@ -38,44 +39,38 @@ impl<T: ApplicationContext + 'static> State<T> {
     pub fn from_display_window(display: Display<WindowSurface>, window: Window) -> Self {
         let context = T::new(&display);
 
-        Self { display, window, context }
+        Self { display, window, context, active: true }
     }
 
     /// Start the event_loop and keep rendering frames until the program is closed
-    pub fn run_loop() {
+    pub fn run_loop() -> Result<(), winit::error::EventLoopError> {
         let event_loop = EventLoopBuilder::new()
             .build()
             .expect("event loop building");
-        let mut state: Option<State<T>> = Some(State::new(&event_loop));
+        let mut state: State<T> = State::new(&event_loop);
 
-        let result = event_loop.run(move |event, window_target| {
+        event_loop.run(move |event, window_target| {
+            if !state.active { () }
+
             match event {
-                Event::Suspended => state = None,
+                Event::Suspended => state.active = false,
                 // By requesting a redraw in response to a AboutToWait event we get continuous rendering.
                 // For applications that only change due to user input you could remove this handler.
                 Event::AboutToWait => {
-                    if let Some(state) = &state {
-                        state.window.request_redraw();
-                    }
+                    state.window.request_redraw();
                 },
                 Event::Resumed => {
                     // set the window size (will call WindowEvent::Resized in the camera)
                     // this is a hack to correctly set the inital aspect ratio for the camera
-                    if let Some(state) = &mut state {
-                        let _ = state.window.request_inner_size(PhysicalSize { width: 800, height: 600 });
-                    }
+                    let _ = state.window.request_inner_size(PhysicalSize { width: 800, height: 600 });
                 },
                 Event::WindowEvent { event, .. } => match event {
                     WindowEvent::Resized(new_size) => {
-                        if let Some(state) = &state {
-                            state.display.resize(new_size.into());
-                        }
+                        state.display.resize(new_size.into());
                     },
                     WindowEvent::RedrawRequested => {
-                        if let Some(state) = &mut state {
-                            state.context.update();
-                            state.context.draw_frame(&state.display);
-                        }
+                        state.context.update();
+                        state.context.draw_frame(&state.display);
                     },
                     // Exit the event loop when requested (by closing the window for example) or when
                     // pressing the Esc key.
@@ -89,14 +84,11 @@ impl<T: ApplicationContext + 'static> State<T> {
                     },
                     // Every other event
                     ev => {
-                        if let Some(state) = &mut state {
-                            state.context.handle_window_event(&ev, &state.window);
-                        }
+                        state.context.handle_window_event(&ev, &state.window);
                     },
                 },
                 _ => (),
             };
-        });
-        result.unwrap();
+        })
     }
 }
